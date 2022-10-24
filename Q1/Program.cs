7a37b5a53d5c328bec1442461af1a666ace67469
ispan <ispan@DESKTOP-PAKSETB>
2022-10-24 13:21:39 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int sum = 0;
+
+			for (int i = 1; i < 150; i++)
+			{
+
+				sum += i;
+
+
+				if (sum > 150)
+				{
+					Console.WriteLine(i);
+					return;
+				}
+			}
+		}
+	}
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int MaxNumber = 300;
+			int MiniNumber = 1;
+
+
+
+			while (MaxNumber >= MiniNumber)
+			{
+				if (MaxNumber % MiniNumber == 0)
+				{
+					Console.WriteLine(MiniNumber);
+
+				}
+				MiniNumber += 1;
+			}
+		}
+	}
+}
